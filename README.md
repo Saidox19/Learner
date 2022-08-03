082c2f90522d08dc76d912ef2bd8377e0a7f0883
Abdulahi Saheed <abdulahisaheed001@gmail.com>
2022-08-03 12:44:09 +0100

    Created README.md
    This is a repository showing how to build a repo without cloning from the begining

---
+++ README.md
@@ -0,0 +1,4 @@
+# Repo Without Clone
+
+
+Trying to create a repo without cloning from the beginning.
---
+++ weight.py
@@ -0,0 +1,5 @@
+weight = [60.2, 48.4, 63.1, 79.2, 71.5]
+
+print(max(weight))
+print(min(weight))
+print(mean(weight))
