edcf30ec631ec3fe9d6a653a11d0f181111c83ae
Abdulahi Saheed <abdulahisaheed001@gmail.com>
2022-08-09 15:08:36 +0100

    Updating exisitng files
    I just updated the file in my local machine

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 Trying to create a repo without cloning from the beginning.
+
+I need to do a few update in the document
--- weight.py
+++ weight.py
@@ -1,5 +1,18 @@
+from audioop import avg
+from gettext import install
+
+
 weight = [60.2, 48.4, 63.1, 79.2, 71.5]
 
 print(max(weight))
 print(min(weight))
-print(mean(weight))
+
+#Define a function
+def average(a):
+    av = sum(a)
+    lt = len(a)
+    cd = av / lt
+    return cd
+
+
+print(average(weight))
